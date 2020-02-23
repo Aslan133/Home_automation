a4cd6897416442aecbb58cc556367f21d37f2959
Aslan133 <54281312+Aslan133@users.noreply.github.com>
2020-02-23 11:20:42 +0200

    update

--- VisualStudio/Home_automation/Home_automation/MainWindow.xaml.cs
+++ VisualStudio/Home_automation/Home_automation/MainWindow.xaml.cs
@@ -22,98 +22,62 @@ namespace Home_automation
     /// </summary>
     public partial class MainWindow : Window
     {
-        const int PORT_NO = 23;
-        const string SERVER_IP = "192.168.0.10";
+        TcpClient client;
+        NetworkStream nwStream;
 
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
 
         private void ConnectToArduinoServerBtn_Click(object sender, RoutedEventArgs e)
         {
-            //Connect("192.168.0.10", "led=1<LF>");
-
-            
-
-            //Byte[] data = System.Text.Encoding.ASCII.GetBytes("led=1<LF>");
-
-
-
-            //---data to send to the server---
-            string textToSend = "led=1<LF>";
-
-            //---create a TCPClient object at the IP and port no.---
-            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
-            NetworkStream nwStream = client.GetStream();
-            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
-
-            //---send the text---
-           // Console.WriteLine("Sending : " + textToSend);
-            ArduinoSendDataLbl.Content = "Sending : " + textToSend;
-            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-
-            //---read back the text---
-            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-            //Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-            ArduinoSendDataLbl.Content = "Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
-            //Console.ReadLine();
-            client.Close();
-        }
-        static void Connect(String server, String message)
-        {
+            const int PORT_NO = 23;
+            const string SERVER_IP = "192.168.0.10";
             try
             {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
-                Int32 port = 23;
-                TcpClient client = new TcpClient(server, port);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);
-
-                // Receive the TcpServer.response.
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
+                //---create a TCPClient object at the IP and port no.---
+                client = new TcpClient(SERVER_IP, PORT_NO);
+                nwStream = client.GetStream();
+                ConnectToArduinoServerBtn.Background = new SolidColorBrush(Color.FromArgb(255, 4, 255, 88));
             }
-            catch (ArgumentNullException e)
+            catch (ArgumentNullException)
             {
-                Console.WriteLine("ArgumentNullException: {0}", e);
+                throw new System.ArgumentNullException("Parameter cannot be null", "original");
             }
-            catch (SocketException e)
+            catch (SocketException)
             {
-                Console.WriteLine("SocketException: {0}", e);
+                throw new SocketException();
             }
 
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
+        }
+        private void SendToArduino(string toSend)
+        {
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(toSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+        private void ReceiveFromArduino()
+        {
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+        }
+
+        private void DisconnectToArduinoServerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            nwStream.Close();
+            client.Close();
+            ConnectToArduinoServerBtn.Background = new SolidColorBrush(Color.FromArgb(255, 221, 221, 221));
+        }
+
+        private void ArduinoLedOnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SendToArduino("on0");
+        }
+
+        private void ArduinoLedOffBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SendToArduino("off0");
         }
     }
 }
